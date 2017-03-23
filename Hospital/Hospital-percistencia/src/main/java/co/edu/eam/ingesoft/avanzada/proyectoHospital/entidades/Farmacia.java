8ca69a2d0af2505bd0dc50d4b65591f538e47a69
Didier_Narváez <Didier_Narvez@Didier-Narvez>
2017-03-22 19:59:31 -0500

    Lucho la dirty

---
+++ Hospital/Hospital-percistencia/src/main/java/co/edu/eam/ingesoft/avanzada/proyectoHospital/entidades/Farmacia.java
@@ -0,0 +1,12 @@
+package co.edu.eam.ingesoft.avanzada.proyectoHospital.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_FARMACIA")
+public class Farmacia implements Serializable {
+
+}
---
+++ Hospital/Hospital-percistencia/src/main/java/co/edu/eam/ingesoft/avanzada/proyectoHospital/entidades/Patologia.java
@@ -0,0 +1,12 @@
+package co.edu.eam.ingesoft.avanzada.proyectoHospital.entidades;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="T_PATOLOGIA")
+public class Patologia implements Serializable {
+
+}
