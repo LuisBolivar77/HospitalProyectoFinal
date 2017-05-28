61f2dd18863a0b1cf25766800ba200f639d0ff7c
davidRomaan <romanleon2010@hotmail.com>
2017-05-28 12:09:10 -0500

    didier tragon

--- Hospital/Hospital-negocio/src/main/java/co/edu/eam/ingesoft/avanzada/negocio/beans/InstalacionesEJB.java
+++ Hospital/Hospital-negocio/src/main/java/co/edu/eam/ingesoft/avanzada/negocio/beans/InstalacionesEJB.java
@@ -17,64 +17,97 @@ import co.edu.eam.ingesoft.avanzada.proyectoHospital.entidades.Quirofano;
 @LocalBean
 @Stateless
 public class InstalacionesEJB {
-	
+
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	/**
 	 * Registra un quirofano en la base de datos
-	 * @param q quirofano que se desea registrar
+	 * 
+	 * @param q
+	 *            quirofano que se desea registrar
 	 */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public void registrarQuirofano (Quirofano q){
+	public void registrarQuirofano(Quirofano q) {
 		Quirofano buscado = buscarQuirofano(q.getNumero());
-		if (buscado == null){
-		em.persist(q);
+		if (buscado == null) {
+			em.persist(q);
 		} else {
 			throw new ExcepcionNegocio("Este quirófano ya se encuentra registrado");
 		}
 	}
-	
+
 	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
-	public Cama buscarCama (int id){
+	public Cama buscarCama(int id) {
 		return em.find(Cama.class, id);
 	}
-	
+
 	/**
 	 * Busca un quirofano
-	 * @param numero número del quirofano que se desea buscar
+	 * 
+	 * @param numero
+	 *            número del quirofano que se desea buscar
 	 * @return el quirófano si lo encuentra, de lo contrario null
 	 */
 	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
-	public Quirofano buscarQuirofano (int numero){
+	public Quirofano buscarQuirofano(int numero) {
 		return em.find(Quirofano.class, numero);
 	}
-	
+
 	/**
 	 * Edita un quirofano
-	 * @param q quirofano que se desea editar
+	 * 
+	 * @param q
+	 *            quirofano que se desea editar
 	 */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public void editarQuirofano (Quirofano q){
+	public void editarQuirofano(Quirofano q) {
 		em.merge(q);
 	}
-	
+
 	/**
 	 * Elimina un quirófano
-	 * @param q quirófano que se desea eliminar
+	 * 
+	 * @param q
+	 *            quirófano que se desea eliminar
 	 */
 	@TransactionAttribute(TransactionAttributeType.REQUIRED)
-	public void eliminarQuirofano (Quirofano q){
-		em.remove(q);
+	public void eliminarQuirofano(Quirofano q) {
+		em.remove(em.merge(q));
+	
 	}
 
 	/**
 	 * lista de las camas disponibles
+	 * 
 	 * @return la lista
 	 */
-	public List<Cama>listarCamas(){
+	public List<Cama> listarCamas() {
 		Query q = em.createNamedQuery(Cama.listarCamasDisponibles);
-		List<Cama>lista = q.getResultList();
+		List<Cama> lista = q.getResultList();
+		return lista;
+	}
+
+	/**
+	 * lista de todos los quirofanos
+	 * @return la lista 
+	 */
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public List<Quirofano> listarQuirofanos() {
+		Query q = em.createNamedQuery(Quirofano.ListarQuirofanos);
+		List<Quirofano> lista = q.getResultList();
+		return lista;
+	}
+	
+	/**
+	 * lista de un quirofano
+	 * @return
+	 */
+	@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
+	public List<Quirofano>quirofanoPorNumero(int num){
+		Query q = em.createNamedQuery(Quirofano.quirofanoPorNumero);
+		q.setParameter(1, num);
+		List<Quirofano>lista = q.getResultList();
 		return lista;
 	}
 }
--- Hospital/Hospital-web/src/main/java/co/edu/eam/ingesoft/avanzada/proyectoHospital/controladores/ControladorInstalaciones.java
+++ Hospital/Hospital-web/src/main/java/co/edu/eam/ingesoft/avanzada/proyectoHospital/controladores/ControladorInstalaciones.java
@@ -2,7 +2,9 @@ package co.edu.eam.ingesoft.avanzada.proyectoHospital.controladores;
 
 import java.io.Serializable;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.omnifaces.cdi.ViewScoped;
@@ -34,19 +36,31 @@ public class ControladorInstalaciones implements Serializable {
 	@EJB
 	private InstalacionesEJB instalacionesEJB;
 
+	private int estatica;
+
+	@PostConstruct
+	public void postconstructor() {
+		cargaDatosQuirofano();
+	}
+
 	/**
 	 * Registra un quirófano en la base de datos
 	 */
 	public void registarQuirofano() {
-		Quirofano q = new Quirofano();
-		q.setOcupado(false);
-		q.setDescripcion(descQuirofano);
-		q.setNumero(numeroQuirofano);
-		try {
-			instalacionesEJB.registrarQuirofano(q);
-			Messages.addFlashGlobalInfo("Registro exitoso");
-		} catch (ExcepcionNegocio e) {
-			Messages.addFlashGlobalError(e.getMessage());
+		if (numeroQuirofano == 0 || descQuirofano == "") {
+			Messages.addFlashGlobalInfo("Ingrese todos los campos");
+		} else {
+			Quirofano q = new Quirofano();
+			q.setOcupado(false);
+			q.setDescripcion(descQuirofano);
+			q.setNumero(numeroQuirofano);
+			cancelarRegistro();
+			try {
+				instalacionesEJB.registrarQuirofano(q);
+				Messages.addFlashGlobalInfo("Registro exitoso");
+			} catch (ExcepcionNegocio e) {
+				Messages.addFlashGlobalError(e.getMessage());
+			}
 		}
 	}
 
@@ -56,33 +70,52 @@ public class ControladorInstalaciones implements Serializable {
 	public void buscarQuirofano() {
 		Quirofano q = instalacionesEJB.buscarQuirofano(numeroQuirofano);
 		if (q != null) {
-			quirofanoBuscado = q;
+			numeroQuirofano = q.getNumero();
 			descQuirofano = q.getDescripcion();
+			Messages.addFlashGlobalInfo("quirofano encontrado");
 		} else {
 			Messages.addFlashGlobalError("Este quirófano no se encuentra registrado");
 		}
 	}
 
 	/**
-	 * Edita un quirófano
+	 * edita un quirofano
 	 */
 	public void editarQuirofano() {
-		if (quirofanoBuscado == null) {
-			Messages.addFlashGlobalError("Debe buscar antes de editar");
+		if (numeroQuirofano == 0) {
+			Messages.addFlashGlobalInfo("busque previa mente un quirofano");
 		} else {
+			quirofanoBuscado = instalacionesEJB.buscarQuirofano(numeroQuirofano);
+			quirofanoBuscado.setNumero(numeroQuirofano);
+			quirofanoBuscado.setDescripcion(descQuirofano);
 			instalacionesEJB.editarQuirofano(quirofanoBuscado);
+			Messages.addFlashGlobalInfo("quirofano editado correctamente");
 		}
 	}
 
 	/**
-	 * Elimina un quirófano
+	 * carga los campos previamente
 	 */
-	public void eliminarQuirofano() {
-		if (quirofanoBuscado == null){
-			Messages.addFlashGlobalError("Debe buscar antes de eliminar");
+	public String cargaDatosQuirofano() {
+		int numero = Quirofano.getNumeroQuirofano();
+		if (numero == 0) {
+			return "/paginas/Administrador/GestionQuirofano.xhtml?faces-redirect=true";
 		} else {
-			instalacionesEJB.eliminarQuirofano(quirofanoBuscado);
+			estatica = Quirofano.getNumeroQuirofano();
+			Quirofano q = instalacionesEJB.buscarQuirofano(numero);
+			numeroQuirofano = q.getNumero();
+			descQuirofano = q.getDescripcion();
+
 		}
+		return null;
+	}
+
+	/**
+	 * cancela un registro de un quirofano
+	 */
+	public void cancelarRegistro() {
+		numeroQuirofano = 0;
+		descQuirofano = "";
 	}
 
 	/**
@@ -113,6 +146,14 @@ public class ControladorInstalaciones implements Serializable {
 	 */
 	public void setDescQuirofano(String descQuirofano) {
 		this.descQuirofano = descQuirofano;
+	}
+
+	public int getEstatica() {
+		return estatica;
+	}
+
+	public void setEstatica(int estatica) {
+		this.estatica = estatica;
 	}
 
 }
