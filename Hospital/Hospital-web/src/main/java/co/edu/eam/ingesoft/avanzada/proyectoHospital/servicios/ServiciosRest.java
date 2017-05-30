3a1d7a03133d8c07d632c5f54263cb7549507bff
Lucho <tennluis77@gmail.com>
2017-05-29 21:26:09 -0500

    Commit Servicio Login 2

---
+++ Hospital/Hospital-web/src/main/java/co/edu/eam/ingesoft/avanzada/proyectoHospital/servicios/ServiciosRest.java
@@ -0,0 +1,11 @@
+package co.edu.eam.ingesoft.avanzada.proyectoHospital.servicios;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ServiciosRest extends Application {
+
+
+	
+}
